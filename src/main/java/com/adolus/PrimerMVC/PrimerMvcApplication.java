b2d53771d537b6f59d36bd404d3dc7ab37e862f6
Adolus <Adolus@DELLADOLUS>
2022-05-11 13:36:11 -0500

    Master comit

---
+++ src/main/java/com/adolus/PrimerMVC/PrimerMvcApplication.java
@@ -0,0 +1,13 @@
+package com.adolus.PrimerMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adolus/PrimerMVC/PrimerMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adolus.PrimerMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
